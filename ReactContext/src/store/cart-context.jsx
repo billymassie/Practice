e3ab31c160f9a389ca658a650abf8a7f3bce1b85
billy.massie <billybois68@gmail.com>
2024-05-01 10:07:38 +0700

    feat: creating cart context

---
+++ ReactContext/src/store/cart-context.jsx
@@ -0,0 +1,14 @@
+import { createContext } from 'react';
+
+export const CartContext = createContext({
+  items: [],
+  addToCart: () => {},
+});
+
+/* 
+the CartContext has a default object that contain 2 key-value pair(properties)
+which: 
+- items -> it is an array that will contain data
+- addToCart -> is the function that will change the data
+this CartContext needs to bind/link with useState
+*/
